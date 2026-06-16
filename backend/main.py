50d48bee1ef64727b47cfa341ebde69320e8b0ba
dejan573 <dejanjovicjovic5@gmail.com>
2026-06-16 07:26:20 +0200

    Fix sitemap and robots headers

--- backend/main.py
+++ backend/main.py
@@ -151,8 +151,7 @@ def robots_txt():
 
     return FileResponse(
         path=str(robots_file),
-        media_type="text/plain; charset=utf-8",
-        filename="robots.txt",
+        media_type="text/plain",
     )
 
 
@@ -165,8 +164,7 @@ def sitemap_xml():
 
     return FileResponse(
         path=str(sitemap_file),
-        media_type="application/xml; charset=utf-8",
-        filename="sitemap.xml",
+        media_type="application/xml",
     )
 
 
